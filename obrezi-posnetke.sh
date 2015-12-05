ed0fa7adc1262bd6456db359113c4c2a85560eea
R33D3M33R <R33D3M33R@users.noreply.github.com>
2015-12-05 20:17:23 +0100

    Dodana skripta za obrezovanje posnetkov

---
+++ obrezi-posnetke.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Author: Andrej Mernik, 2015, http://andrej.mernik.eu/brezplacne-igre-za-ubuntu/
+#License: GPLv3
+
+dimenzije="800x600" # sirina x visina obrezane slike
+izhodna_mapa="obrezano" # podmapa, v katero bodo shranjene obrezane slike
+
+type convert >/dev/null 2>&1 || { echo >&2 "Program imagemagick je zahtevan in ni namescen!"; exit 1; }
+if [ ! -d "$izhodna_mapa" ]; then
+    echo "Ustvarjanje izhodne mape $izhodna_mapa ..."
+    mkdir "$izhodna_mapa"
+fi
+if [ $# -eq 1 ]; then
+    if [[ "$1" =~ ^[0-9]+[xX][0-9]+$ ]]; then
+        dimenzije="$1"
+    fi
+fi
+
+echo "Rezanje zaslonskih slik ..."
+for slika in *.png; do
+     convert $slika -crop "$dimenzije"+0+0 +repage "$izhodna_mapa/$slika"
+done
