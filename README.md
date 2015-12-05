5cbf4092aafc5c73afdb7297349110b5a0c7477d
R33D3M33R <R33D3M33R@users.noreply.github.com>
2015-12-05 19:31:30 +0100

    Posodobljen readme +  dodana skripta za zajem zaslona

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # brezplacne-igre-za-ubuntu
 Skripte za obdelavo seznama brezplačnih iger za (K)Ubuntu
 http://andrej.mernik.eu/igre/brezplacne-igre-za-ubuntu/
+
+# zajem-zaslona.sh
+Skripta za zajem zaslona, ki uporablja program scrot
+
+## Uporaba:
+```bash
+bash zajem-zaslona.sh igra
+```
---
+++ zajem-zaslona.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+i=0
+zakasnitev=30 # koliko sekund naj mine med posnetki
+
+type scrot >/dev/null 2>&1 || { echo >&2 "Program scrot je zahtevan in ni namescen!"; exit 1; }
+
+if [ $# -eq 1 ]; 
+  then igra=$1
+else
+  igra="posnetek"
+fi
+echo "Zajemanje zaslonskih slik ..."
+while [ $i -lt 240 ]
+do
+  let "i++"
+  ime="$igra$(date +"_%H%M%S_")$i".png
+  scrot -d $zakasnitev $ime
+  echo "Zajet posnetek $ime"
+done
