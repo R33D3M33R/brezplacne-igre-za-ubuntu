899777f57b1f90c259f979c4f65573b9202cf084
R33D3M33R <R33D3M33R@users.noreply.github.com>
2015-12-05 19:46:52 +0100

    Poenostavitev skripte

--- zajem-zaslona.sh
+++ zajem-zaslona.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 i=0
 zakasnitev=30 # koliko sekund naj mine med posnetki
+posnetek="posnetek" # privzeto ime za posnetek
 
 type scrot >/dev/null 2>&1 || { echo >&2 "Program scrot je zahtevan in ni namescen!"; exit 1; }
 
-if [ $# -eq 1 ]; 
-  then igra=$1
-else
-  igra="posnetek"
+if [ $# -eq 1 ]; then
+    posnetek="$1"
 fi
 echo "Zajemanje zaslonskih slik ..."
 while [ $i -lt 240 ]
 do
   let "i++"
-  ime="$igra$(date +"_%H%M_")$i".png
+  ime="$posnetek$(date +"_%H%M_")$i".png
   scrot -d $zakasnitev $ime
   echo "Zajet posnetek $ime"
 done
