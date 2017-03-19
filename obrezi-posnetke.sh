9def08d3585516204e53bb6a680c3c7cf508aa03
Andrej Mernik <R33D3M33R@users.noreply.github.com>
2017-03-19 09:11:04 +0100

    Add subfolder support

--- obrezi-posnetke.sh
+++ obrezi-posnetke.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#Author: Andrej Mernik, 2015, http://andrej.mernik.eu/brezplacne-igre-za-ubuntu/
+#Author: Andrej Mernik, 2015-2017, http://andrej.mernik.eu/brezplacne-igre-za-ubuntu/
 #License: GPLv3
 
 dimenzije="800x600" # sirina x visina obrezane slike
+vhodna_mapa="zajem" # mapa, v kateri so shranjene izvirne slike
 izhodna_mapa="obrezano" # podmapa, v katero bodo shranjene obrezane slike
 
 type convert >/dev/null 2>&1 || { echo >&2 "Program imagemagick je zahtevan in ni namescen!"; exit 1; }
@@ -15,6 +16,7 @@ if [[ "$#" -eq 1 && "$1" =~ ^[0-9]+[xX][0-9]+$ ]]; then
 fi
 
 echo "Rezanje zaslonskih slik ..."
-for slika in *.png; do
-     convert $slika -crop "$dimenzije"+0+0 +repage "$izhodna_mapa/$slika"
+for pot_do_slike in "$vhodna_mapa"/*.png; do
+    slika=$(basename $pot_do_slike)
+    convert $pot_do_slike -crop "$dimenzije"+0+0 +repage "$izhodna_mapa/$slika"
 done
