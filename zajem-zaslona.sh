ca5e13a6573db4ea6c452b94837734db0b01dbe4
Andrej Mernik <R33D3M33R@users.noreply.github.com>
2017-03-19 09:13:39 +0100

    Add subfolder support, author http -> https

--- zajem-zaslona.sh
+++ zajem-zaslona.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#Author: Andrej Mernik, 2015, http://andrej.mernik.eu/brezplacne-igre-za-ubuntu/
+#Author: Andrej Mernik, 2015-2017, https://andrej.mernik.eu/brezplacne-igre-za-ubuntu/
 #License: GPLv3
 
 i=0
 zakasnitev=30 # koliko sekund naj mine med posnetki
 posnetek="posnetek" # privzeto ime za posnetek
+mapa="zajem" # privzeta mapa za zajem, "." za trenutno mapo
 
 type scrot >/dev/null 2>&1 || { echo >&2 "Program scrot je zahtevan in ni namescen!"; exit 1; }
 
@@ -25,6 +26,6 @@ while [ $i -lt 240 ]
 do
   let "i++"
   ime="$posnetek$(date +"_%H%M_")$i".png
-  scrot -z -d $zakasnitev $ime
+  scrot -z -d $zakasnitev "$mapa/$ime"
   echo "Zajet posnetek $ime"
 done
